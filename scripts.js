17ed72bc1422917d9640f74d7096850d30eada5b
Devvrat Singh Rathod <73027078+Devvrat1010@users.noreply.github.com>
2023-04-20 22:41:05 +0530

    added / after co

--- scripts.js
+++ scripts.js
@@ -6,7 +6,7 @@ const btn=document.getElementById("btn")
 
 const fetchbio=(height,weight)=>{
     // res=
-    fetch("https://bmi-calculator-api.devvrat-singhsi.repl.co?height="+height+"&weight="+weight)
+    fetch("https://bmi-calculator-api.devvrat-singhsi.repl.co/?height="+height+"&weight="+weight)
     .then(res=>res.json())
     .then(res=>container.innerText=res["output"])
 };
